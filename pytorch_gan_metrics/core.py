b056a8ab853f4dcbc1929d439cd40511c9424ab2
Had <had-95@yandex.ru>
2022-04-28 23:17:55 +0300

    refactor get_inception_feature a bit

--- pytorch_gan_metrics/core.py
+++ pytorch_gan_metrics/core.py
@@ -11,11 +11,10 @@ from .inception import InceptionV3
 
 # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-
+@torch.no_grad()
 def get_inception_feature(
-    images: Union[List[torch.FloatTensor], DataLoader],
+    dataloader: DataLoader,
     dims: List[int],
-    batch_size: int = 50,
     use_torch: bool = False,
     verbose: bool = False,
     device: torch.device = torch.device('cuda:0'),
@@ -25,13 +24,10 @@ def get_inception_feature(
     calculating features for FID and Inception Score.
 
     Args:
-        images: List of tensor or torch.utils.data.Dataloader. The return image
+        dataloader: torch.utils.data.Dataloader. The return image
             must be float tensor of range [0, 1].
         dims: List of int, see InceptionV3.BLOCK_INDEX_BY_DIM for
             available dimension.
-        batch_size: int, The batch size for calculating activations. If
-            `images` is torch.utils.data.Dataloader, this argument is
-            ignored.
         use_torch: bool. The default value is False and the backend is same as
             official implementation, i.e., numpy. If use_torch is enableb,
             the backend linalg is implemented by torch, the results are not
@@ -46,47 +42,19 @@ def get_inception_feature(
     """
     assert all(dim in InceptionV3.BLOCK_INDEX_BY_DIM for dim in dims)
 
-    is_dataloader = isinstance(images, DataLoader)
-    if is_dataloader:
-        num_images = min(len(images.dataset), images.batch_size * len(images))
-        batch_size = images.batch_size
-    else:
-        num_images = len(images)
-
     block_idxs = [InceptionV3.BLOCK_INDEX_BY_DIM[dim] for dim in dims]
     model = InceptionV3(block_idxs).to(device)
     model.eval()
+    features = [[] for dim in dims]
 
-    if use_torch:
-        features = [torch.empty((num_images, dim)).to(device) for dim in dims]
-    else:
-        features = [np.empty((num_images, dim)) for dim in dims]
-
-    pbar = tqdm(
-        total=num_images, dynamic_ncols=True, leave=False,
-        disable=not verbose, desc="get_inception_feature")
-    looper = iter(images)
-    start = 0
-    while start < num_images:
-        # get a batch of images from iterator
-        if is_dataloader:
-            batch_images = next(looper)
-        else:
-            batch_images = images[start: start + batch_size]
-        end = start + len(batch_images)
-
-        # calculate inception feature
-        batch_images = batch_images.to(device)
-        with torch.no_grad():
-            outputs = model(batch_images)
-            for feature, output, dim in zip(features, outputs, dims):
-                if use_torch:
-                    feature[start: end] = output.view(-1, dim)
-                else:
-                    feature[start: end] = output.view(-1, dim).cpu().numpy()
-        start = end
-        pbar.update(len(batch_images))
-    pbar.close()
+    for batch_images in tqdm(dataloader, dynamic_ncols=True, leave=False,
+        disable=not verbose, desc="get_inception_feature"):
+        outputs = model(batch_images.to(device))
+        for feature, output in zip(features, outputs):
+            feature.append(output.squeeze(-1).squeeze(-1))
+    features = [torch.cat(feature, dim=0) for feature in features]
+    if not use_torch:
+        features = [feature.cpu.numpy() for feature in features]
     return features
 
 
